9f038b6dd31a64d0a7f4b90eb34fa8b3f6adc643
elija-xp <eyelja@gmail.com>
2026-05-01 19:34:28 +0300

    fix review issues

--- services/order.py
+++ services/order.py
@@ -1,5 +1,3 @@
-import datetime
-
 from django.contrib.auth import get_user_model
 from django.db import transaction
 from django.db.models import QuerySet
@@ -11,7 +9,7 @@ from db.models import Order, Ticket
 def create_order(
         tickets: list[dict],
         username: str,
-        date: datetime = None
+        date: str = None
 ) -> Order:
     user = get_user_model().objects.get(username=username)
     order = Order.objects.create(user=user)
